cd488e06530190559825791ecbeee8aecd5b15e9
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2023-02-05 11:12:46 +0530

    Basic UI and Layouts added

---
+++ app/src/main/java/com/jacinthmahanta/calculator/Calculator.kt
@@ -0,0 +1,11 @@
+package com.jacinthmahanta.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
