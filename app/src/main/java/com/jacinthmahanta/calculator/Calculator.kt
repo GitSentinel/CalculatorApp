0ac69c238a46f2d219be95bcfd9efa8844ffdf89
Debashish Kundu <debashish.joy@gmail.com>
2023-10-02 21:48:44 +0530

    impl viewbinding, optimised code

--- app/src/main/java/com/jacinthmahanta/calculator/Calculator.kt
+++ app/src/main/java/com/jacinthmahanta/calculator/Calculator.kt
@@ -1,143 +1,80 @@
 package com.jacinthmahanta.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.jacinthmahanta.calculator.databinding.ActivityCalculatorBinding
 
 class Calculator : AppCompatActivity() {
+    private lateinit var binding: ActivityCalculatorBinding
 
-    lateinit var number1: String
-    lateinit var number2: String
-    lateinit var operation: String
-    lateinit var result: TextView
+    private lateinit var number1: String
+    private lateinit var number2: String
+    private lateinit var operation: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calculator)
-        number1=""
-        number2=""
-        operation=""
-        result = findViewById<TextView>(R.id.result)
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val n1 = findViewById<Button>(R.id.n1)
-        val n2 = findViewById<Button>(R.id.n2)
-        val n3 = findViewById<Button>(R.id.n3)
-        val n4 = findViewById<Button>(R.id.n4)
-        val n5 = findViewById<Button>(R.id.n5)
-        val n6 = findViewById<Button>(R.id.n6)
-        val n7 = findViewById<Button>(R.id.n7)
-        val n8 = findViewById<Button>(R.id.n8)
-        val n9 = findViewById<Button>(R.id.n9)
-        val n0 = findViewById<Button>(R.id.n0)
-        val plus = findViewById<Button>(R.id.plus)
-        val minus = findViewById<Button>(R.id.minus)
-        val divide = findViewById<Button>(R.id.divide)
-        val multiply = findViewById<Button>(R.id.multiply)
-        val equals = findViewById<Button>(R.id.equal)
-        n1.setOnClickListener {
-            SetNumber("1")
-        }
-        n2.setOnClickListener {
-            SetNumber("2")
-        }
-        n3.setOnClickListener {
-            SetNumber("3")
-        }
-        n4.setOnClickListener {
-            SetNumber("4")
-        }
-        n5.setOnClickListener {
-            SetNumber("5")
-        }
-        n6.setOnClickListener {
-            SetNumber("6")
-        }
-        n7.setOnClickListener {
-            SetNumber("7")
-        }
-        n8.setOnClickListener {
-            SetNumber("8")
-        }
-        n9.setOnClickListener {
-            SetNumber("9")
-        }
-        n0.setOnClickListener {
-            SetNumber("0")
-        }
-        plus.setOnClickListener {
-            SetOpration("+")
-        }
-        minus.setOnClickListener {
-            SetOpration("-")
-        }
-        divide.setOnClickListener {
-            SetOpration("/")
-        }
-        multiply.setOnClickListener {
-            SetOpration("*")
-        }
-        equals.setOnClickListener {
-            calculate()
-        }
+        number1 = ""
+        number2 = ""
+        operation = ""
 
+        binding.apply {
+            n1.setOnClickListener { setNumber("1") }
+            n2.setOnClickListener { setNumber("2") }
+            n3.setOnClickListener { setNumber("3") }
+            n4.setOnClickListener { setNumber("4") }
+            n5.setOnClickListener { setNumber("5") }
+            n6.setOnClickListener { setNumber("6") }
+            n7.setOnClickListener { setNumber("7") }
+            n8.setOnClickListener { setNumber("8") }
+            n9.setOnClickListener { setNumber("9") }
+            n0.setOnClickListener { setNumber("0") }
+            plus.setOnClickListener { setOperation("+") }
+            minus.setOnClickListener { setOperation("-") }
+            divide.setOnClickListener { setOperation("/") }
+            multiply.setOnClickListener { setOperation("*") }
+            equal.setOnClickListener { calculate() }
+        }
     }
 
-    fun SetNumber(number: String){
-        if(operation=="")
-        {
-            number1=number1+number
+    private fun setNumber(number: String) {
+        if (operation == "") {
+            number1 += number
             updateUI(number1)
-        }
-        else{
-            number2 = number2 + number
+        } else {
+            number2 += number
             updateUI(number2)
         }
     }
 
-    fun updateUI(res: String){
-        result.text = res
+    private fun updateUI(res: String) {
+        binding.result.text = res
     }
 
-    fun SetOpration(operationtext: String){
-        if(operation=="")
-        {
-            operation = operationtext
+    private fun setOperation(operationText: String) {
+        if (operation == "") {
+            operation = operationText
             updateUI("")
-        }
-        else{
-            val toast = Toast.makeText(applicationContext,"Operation Not Completed", Toast.LENGTH_LONG)
-            toast.show()
-        }
+        } else Toast.makeText(applicationContext, "Operation Not Completed", Toast.LENGTH_LONG)
+            .show()
     }
 
-    fun calculate(){
-        if(number1=="" ||number2=="" || operation==""){
-            val toast = Toast.makeText(applicationContext,"Invalid Operation", Toast.LENGTH_LONG)
-            toast.show()
+    private fun calculate() {
+        if (number1 == "" || number2 == "" || operation == "")
+            Toast.makeText(applicationContext, "Invalid Operation", Toast.LENGTH_LONG).show()
+
+        binding.result.text = when (operation) {
+            "+" -> (number1.toInt() + number2.toInt()).toString()
+            "-" -> (number1.toInt() - number2.toInt()).toString()
+            "/" -> (number1.toInt() / number2.toInt()).toString()
+            else -> (number1.toInt() * number2.toInt()).toString()
         }
 
-        if(operation == "+")
-        {
-            result.text = (number1.toInt() + number2.toInt()).toString()
-        }
-        else if(operation == "-")
-        {
-            result.text = (number1.toInt() - number2.toInt()).toString()
-        }
-        else if(operation == "/")
-        {
-            result.text = (number1.toInt() / number2.toInt()).toString()
-        }
-        else if(operation == "*")
-        {
-            result.text = (number1.toInt() * number2.toInt()).toString()
-        }
-
-        number1 = result.text.toString()
-        operation=""
-        number2=""
+        number1 = binding.result.text.toString()
+        operation = ""
+        number2 = ""
     }
-
-
 }
